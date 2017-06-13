24a38497eb14396fb3765cd8a5e342c205e564e2
andrea.sanchez <andrea.sanchez@amadeus.com>
2017-06-13 17:23:40 -0500

    Carga  modal personajes

--- js/default.js
+++ js/default.js
@@ -28,7 +28,7 @@ var urls= "http://gateway.marvel.com/v1/public/characters?orderBy=name&apikey=39
                         .append(element.description)
                       
                       )
-                       .append($('<a class="more" onclick="modal('+element.id+')">')
+                       .append($('<a data-remodal-target="modal" class="more" onclick="modal('+element.id+')">')
 
                         .append('View More')
 
@@ -58,18 +58,38 @@ var urls= "http://gateway.marvel.com/v1/public/characters?orderBy=name&apikey=39
 
 
 function modal(id){
-var urls ="https://gateway.marvel.com:443/v1/public/characters/"+id+"?&apikey=39e4791e9527c9f646758dc6e5e5a9b4&ts=1&hash=b03a121752073434b2cd0be9ac6df552";
+ var urls ="https://gateway.marvel.com:443/v1/public/characters/"+id+"?&apikey=39e4791e9527c9f646758dc6e5e5a9b4&ts=1&hash=b03a121752073434b2cd0be9ac6df552";
+  
+    
  
+ 
+  
 $.ajax({
     url: urls,
     method: "GET"
    }).done(function(response) {
      $.each(response.data.results, function(index, element) {
 console.log(element);
+$('div.remodal').empty();
+ $("div.remodal")
+         .append($('<div class="row info">')
+                  .append($('<div class="col-md-4 left" id="imgico">')
+                      .append($('<img>')
+                        .attr('src', element.thumbnail.path + '/portrait_fantastic.' + element.thumbnail.extension)
+                      )
+                  )
+                  .append($('<div class="col-md-8 right"><a href="" data-remodal-action="cancel" class="close"></a>')
+                      .append($('<h1>')
+                        .append(element.name)
+                      )
+                      .append($('<p>')
+                     
+                        .append(element.description)
+                      
+                      )) )
+                       .append($('<div class="row options"><div class="col-md-6"><a href="" class="add" onclick="favorite('+element.id+')"><span></span> ADD TO FAVORITES</a></div><div class="col-md-6"><a href="https://comicstore.marvel.com/" class="buy"><span></span> BUY FOR $3.99</a>')
+                      )
      }); 
         });  
-
-   
-          
-        
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
